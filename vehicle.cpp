fd55463df6861f3e9b7ec21095731e673f5f076e
juuxi <dimashopyrev@mail.ru>
2025-08-14 11:50:03 +0300

    Vehicle constructor changed to member initializer list type

--- vehicle.cpp
+++ vehicle.cpp
@@ -1,12 +1,7 @@
 #include "vehicle.h"
 
-Vehicle::Vehicle() {
-    plate = "ABCDEF";
-    model = "Honda Spada";
-    enter_time = QTime(QDateTime::currentDateTime().time());
-    duration = QTime();
-    is_placed_correctly = true;
-}
+Vehicle::Vehicle(): enter_time(QTime(QDateTime::currentDateTime().time())), duration(QTime()),
+    is_placed_correctly(true) {}
 
 void Vehicle::updateDuration() {
     int secs = enter_time.hour() * 3600;
@@ -14,7 +9,6 @@ void Vehicle::updateDuration() {
     secs += enter_time.second();
     duration = QTime(QDateTime::currentDateTime().time()).addSecs(-secs);
 }
-
 
 bool Vehicle::operator==(const Vehicle& other) {
     if (this->plate != other.plate) {
