39befce1014a0a20a599921fdd9ec829f79c966f
juuxi <dimashopyrev@mail.ru>
2025-08-20 21:25:16 +0300

    deleted build/ from cutier GitHub purpose

--- build/Desktop-Debug/CMakeFiles/Parking_System_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Parking_System_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Parking_System_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
